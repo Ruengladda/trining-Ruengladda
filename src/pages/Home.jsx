ddd6232f7d9b4e5a3c464a420f0a2037b675a593
Ruengladda <gigryht2525@gmail.com>
2024-01-14 14:22:40 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home(){
+ return(
+    <div>
+        <h1>หน้าหลัก</h1><hr/>
+        <center><img src="Sai3.png"/></center>
+    </div>
+    
+
+ )
+}
+
+
+export default Home
